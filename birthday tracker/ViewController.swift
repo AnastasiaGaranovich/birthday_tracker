cef5bf6d9e9836e42047ac3cc447558ae159f796
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-08-20 16:26:02 +0300

    Initial Commit

---
+++ birthday tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  birthday tracker
+//
+//  Created by Анастасия Гаранович on 8/20/20.
+//  Copyright © 2020 Анастасия Гаранович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
