333d84c15e07118b6edf924b5291cb6943e574f4
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-09-07 21:10:13 +0300

    first commit

---
+++ birthday tracker/Birthday.swift
@@ -0,0 +1,9 @@
+//
+//  Birthday.swift
+//  birthday tracker
+//
+//  Created by Анастасия Гаранович on 8/27/20.
+//  Copyright © 2020 Анастасия Гаранович. All rights reserved.
+//
+
+import Foundation
